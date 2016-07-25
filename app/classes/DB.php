34232abe73caae963a0890ca8a03a958a80f3a71
Cookie Zhang <ccookia@hotmail.com>
2016-07-25 19:51:29 -0400

    login-with-google-php

--- app/classes/DB.php
+++ app/classes/DB.php
@@ -5,7 +5,7 @@ class DB{
 	protected $mysqli;
 
 	public function __construct(){
-		$this->mysqli = new mysqli('localhost','root','xixi2008','website'); // server, username, password, database name
+		$this->mysqli = new mysqli('localhost','root','root','website'); // server, username, password, database name
 	}
 
 	public function query($sql){
